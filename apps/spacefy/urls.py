fc7665eca4160aeec96a43c635f238744e1f3576
dmytro <rungotika@gmail.com>
2023-11-07 20:17:13 +0100

    my-space aded

--- apps/spacefy/urls.py
+++ apps/spacefy/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views import MySpaceView
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='apps.spacefy/home.html'), name='home')
+    path('', TemplateView.as_view(template_name='apps.spacefy/home.html'), name='home'),
+    path('my-space', MySpaceView.as_view(), name='my-space')
 ]
--- apps/spacefy/views.py
+++ apps/spacefy/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MySpaceView(View):
+    template_name = 'apps.spacefy/my-space.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
